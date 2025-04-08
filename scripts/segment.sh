40c18caf3e6fe7c8af4c6ed90e0a7cbacab72604
xinjian <xinjian.wang>
2025-04-08 02:31:50 -0400

    add segmentation debug json

--- scripts/segment.sh
+++ scripts/segment.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # Activate the YOLO environment
-conda activate yolo
 
 pth_path=${1:-"./pths/yolo11n-seg.pt"}
 img_path=${2:-"./tmp/person.png"}
+output_dir=${3:-"./tmp/output"}
+class_names=${4:-"person"}
+height=${5:-"544"}
+width=${6:-"960"}
 
 # Run the segmentation script
-python scripts/segment.py --model $pth_path --image $img_path
+python scripts/segment.py --model $pth_path --source $img_path --output-dir $output_dir --class-names $class_names --imgsz $height $width
